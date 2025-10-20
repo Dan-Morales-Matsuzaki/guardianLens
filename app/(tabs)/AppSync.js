289c591df1d5893e683e8a8753e1c6b173771440
DanDannDan <tup09729@temple.edu>
2025-10-20 15:44:22 +0900

    changes to the .js

---
+++ app/(tabs)/AppSync.js
@@ -0,0 +1,6 @@
+export default{
+    "graphqlEndpoint": "https://6jg4v7pdybhwjpsnyxvsfljyqa.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "region": "ap-northeast-1",
+    "authenticationType": "API_Key",
+    "apiKey": "da2-lears2uxf5eunibvzzpz2xjtu4"
+}
